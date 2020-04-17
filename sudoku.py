02c4a63196335c922bbfddba36227abd5d0c3fff
wallouf <ct13860@gmail.com>
2020-04-17 18:09:34 +0100

    Update sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -6,6 +6,7 @@ class Application(Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
+        self.entries={}
         
         # self.f1 = Frame(master)
         # self.f1.grid(row=0, column=0)
@@ -13,17 +14,74 @@ class Application(Frame):
         self.pack()
         self.create_widgets()
 
-    def update_entry_value(self, event, text):
+    def generate_sudoku(self, text, posx, posy):
+        return
+
+    def check_sudoku_rule(self, entry, text, posx, posy):
+        # calcul square
+        posx +=1
+        widgetvalue = entry.get()
+        square=1
+
+        offsetx = 0
+        offsety = 0
+
+        if posx > 6:
+            square += 2
+            offsetx = 6
+        elif posx > 3:
+            square += 1
+            offsetx = 3
+
+        if posy > 6:
+            square += 6
+            offsety = 6
+        elif posy > 3:
+            square += 3
+            offsety = 3
+
+        # Calculate position
+        square_offset = ((square - 1) * 9)
+        elem = square_offset + ((posy - offsety - 1) * 3) + (posx - offsetx)
+            
+        print("Check rule...")
+        print(posx)
+        print(posy)
+        print("")
+        print("Element:")
+        print(elem)
+        print("")
+        print("Square:")
+        print(square)
+        print("")
+
+        square_dict = {}
+        range_min = square_offset + 1
+        range_max = square_offset + 10
+
+        for iti1 in range(range_min,range_max):
+            if elem == iti1:
+                continue
+            actual_value = self.entries.get("entry{0}".format(iti1)).get()
+            if actual_value.isdecimal():
+                square_dict[actual_value] = ""
+        
+        # check for square
+        if widgetvalue in square_dict:
+            text.set("")
+            return
+        # check for lines
+
+
+    def update_entry_value(self, event, text, posx, posy):
         # value
         widgetvalue = event.widget.get()
         if not (widgetvalue.isdecimal() and 0 < int(widgetvalue) < 10):
             text.set("")
-        # else:
-        #     self.label["text"] = event.widget.get()
+        else:
+            self.check_sudoku_rule(event.widget, text, posx, posy)
 
     def create_widgets(self, frame=None):
-        self.entriesvalues={}
-        self.entries={}
         self.entrycount = 1
         self.offsetx = 0
         self.offsety = 0
@@ -38,7 +96,8 @@ class Application(Frame):
             
             for self.iti2 in range(0,9):
                 # Each 3 slot, return x to 0 and offset
-                if (self.posx % 3) == 0:
+                self.modulo=(self.posx % 3)
+                if self.modulo != 3 and self.modulo == 0:
                     self.posx = self.offsetx
                     self.posy += 1
                 
@@ -46,14 +105,13 @@ class Application(Frame):
                 self.text = StringVar()
 
                 # Init entry value
-                self.text.set(self.entrycount)
+                self.generate_sudoku(self.text, self.posx, self.posy)
 
-                self.entriesvalues["entryvalue{0}".format(self.entrycount)] = self.text
                 # Create entries
                 self.entry = Entry(self, width=4, textvariable=self.text)
                 self.entry.grid(column=self.posx, row=self.posy)
                 # self.entry.bind('<KeyRelease>', self.update_entry_value)
-                self.entry.bind('<KeyRelease>', lambda event, v=self.text: self.update_entry_value(event, v))
+                self.entry.bind('<KeyRelease>', lambda event, v=self.text, posx=self.posx, posy=self.posy: self.update_entry_value(event, v, posx, posy))
 
                 self.entries["entry{0}".format(self.entrycount)] = self.entry
                 
