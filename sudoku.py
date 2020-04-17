df79f8f86efc92b40b7209cd37473136d895b06d
wallouf <ct13860@gmail.com>
2020-04-17 18:11:02 +0100

    Update sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -142,6 +142,10 @@ class Application(Frame):
             return
 
     def search_good_values(self):
+        entrycount = 1
+        offsetx = 0
+        offsety = 0
+
         cursor = 0
         tester = 0
         possible_values = []
@@ -149,80 +153,117 @@ class Application(Frame):
         for cursor in range(0,81):
             possible_values.append([])
 
-        for cursor in range(1,82):
-            posx = 0
-            posy = 1
-
-            posx +=1
+        for iti1 in range(1,10):
             
-            for tester in range(1,10):
-                # calcul square
-                square=1
-
-                offsetx = 0
-                offsety = 0
-
-                if posx > 6:
-                    square += 2
-                    offsetx = 6
-                elif posx > 3:
-                    square += 1
-                    offsetx = 3
-
-                if posy > 6:
-                    square += 6
-                    offsety = 6
-                elif posy > 3:
-                    square += 3
-                    offsety = 3
-
-                # Calculate position
-                square_offset = ((square - 1) * 9)
-                elem = square_offset + ((posy - offsety - 1) * 3) + (posx - offsetx)
-
-                if elem in self.entries_generated:
+            posx = offsetx
+            posy = offsety
+            print("")
+            print("")
+            print("offsetX:")
+            print(offsetx)
+            print("offsetY:")
+            print(offsety)
+            print("")
+            print("")
+            
+            for iti2 in range(0,9):
+                # Each 3 slot, return x to 0 and offset
+                modulo=((posx+1) % 3)
+                
+                if entrycount in self.entries_generated:
+                    # Increment
+                    entrycount += 1
+                    posx += 1
+                    
+                    if modulo == 0:
+                        posx = offsetx
+                        posy += 1
+                    
                     continue
-
-                square_dict = []
-                range_min = square_offset + 1
-                range_max = square_offset + 10
-
-                for iti1 in range(range_min,range_max):
-                    if elem == iti1:
-                        continue
-                    actual_value = self.entries.get("entry{0}".format(iti1)).get()
-                    if actual_value.isdecimal():
-                        square_dict.append(int(actual_value))
                 
-                # check for lines H
-                h_lines_dict = []
-                h_lines_entries = self.entries_hori[(posy-1)]
-
-                for iti1 in range(0,9):
-                    if (posx-1) == iti1:
-                        continue
-                    actual_value = h_lines_entries.get("entry{0}".format(iti1)).get()
-                    if actual_value.isdecimal():
-                        h_lines_dict.append(int(actual_value))
+                print("")
+                print("")
+                print("Nb:")
+                print(entrycount)
+                print("X:")
+                print(posx)
+                print("Y:")
+                print(posy)
+                print("Mod:")
+                print(modulo)
                 
-                # check for lines V
-                v_lines_dict = []
-                v_lines_entries = self.entries_vert[(posx-1)]
+                for tester in range(1,10):
+                    # calcul square
+                    square=1
 
-                for iti1 in range(0,9):
-                    if (posy-1) == iti1:
-                        continue
-                    actual_value = v_lines_entries.get("entry{0}".format(iti1)).get()
-                    if actual_value.isdecimal():
-                        v_lines_dict.append(int(actual_value))
+                    if posx > 5:
+                        square += 2
+                    elif posx > 2:
+                        square += 1
+
+                    if posy > 5:
+                        square += 6
+                    elif posy > 2:
+                        square += 3
+
+                    # Calculate position
+                    square_offset = ((square - 1) * 9)
+
+                    square_dict = []
+                    range_min = square_offset + 1
+                    range_max = square_offset + 10
+
+                    for iti3 in range(range_min,range_max):
+                        if entrycount == iti3:
+                            continue
+                        actual_value = self.entries.get("entry{0}".format(iti3)).get()
+                        if actual_value.isdecimal():
+                            square_dict.append(int(actual_value))
+                    
+                    # check for lines H
+                    h_lines_dict = []
+                    h_lines_entries = self.entries_hori[(posy)]
+
+                    for iti4 in range(0,9):
+                        if (posx) == iti4:
+                            continue
+                        actual_value = h_lines_entries.get("entry{0}".format(iti4)).get()
+                        if actual_value.isdecimal():
+                            h_lines_dict.append(int(actual_value))
+                    
+                    # check for lines V
+                    v_lines_dict = []
+                    v_lines_entries = self.entries_vert[(posx)]
+
+                    for iti5 in range(0,9):
+                        if (posy) == iti5:
+                            continue
+                        actual_value = v_lines_entries.get("entry{0}".format(iti5)).get()
+                        if actual_value.isdecimal():
+                            v_lines_dict.append(int(actual_value))
+                    
+                    if not ((tester in square_dict) or (tester in v_lines_dict) or (tester in h_lines_dict)):
+                        possible_values[(entrycount-1)].append(tester)
                 
-                if not ((tester in square_dict) or (tester in v_lines_dict) or (tester in h_lines_dict)):
-                    possible_values[cursor].append(tester)
-            
-            print(possible_values[cursor])
-            if len(possible_values[cursor]) == 1:
-                self.entries["text{0}".format(self.cursor)].set(possible_values[cursor][0])
-            break
+                print(possible_values[(entrycount-1)])
+                if len(possible_values[(entrycount-1)]) == 1:
+                    self.entries["text{0}".format((entrycount-1))].set(possible_values[(entrycount-1)][0])
+
+                
+                # Increment
+                entrycount += 1
+                posx += 1
+                
+                if modulo == 0:
+                    posx = offsetx
+                    posy += 1
+
+            # Increment position for next 9 blocs
+            if (iti1 % 3) == 0:
+                offsetx = 0
+                offsety += 3
+            else:
+                offsetx += 3
 
     def update_entry_value(self, event, text, posx, posy):
         # value
