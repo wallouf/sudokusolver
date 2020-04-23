34336e9b249fd903b43dbf0806c07965e4d1b0db
wallouf <ct13860@gmail.com>
2020-04-23 17:25:45 +0100

    login first step

---
+++ website/js/cognito-auth.js
@@ -0,0 +1,106 @@
+/*global WildRydes _config AmazonCognitoIdentity AWSCognito*/
+
+var WildRydes = window.WildRydes || {};
+
+(function scopeWrapper($) {
+    var signinUrl = 'login.html';
+
+    var poolData = {
+        UserPoolId: _config.cognito.userPoolId,
+        ClientId: _config.cognito.userPoolClientId
+    };
+
+    var userPool;
+
+    if (!(_config.cognito.userPoolId &&
+          _config.cognito.userPoolClientId &&
+          _config.cognito.region)) {
+        return;
+    }
+
+    userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+
+    if (typeof AWSCognito !== 'undefined') {
+        AWSCognito.config.region = _config.cognito.region;
+    }
+
+    WildRydes.signOut = function signOut() {
+        userPool.getCurrentUser().signOut();
+    };
+
+    WildRydes.authToken = new Promise(function fetchCurrentAuthToken(resolve, reject) {
+        var cognitoUser = userPool.getCurrentUser();
+
+        if (cognitoUser) {
+            cognitoUser.getSession(function sessionCallback(err, session) {
+                if (err) {
+                    reject(err);
+                } else if (!session.isValid()) {
+                    resolve(null);
+                } else {
+                    resolve(session.getIdToken().getJwtToken());
+                }
+            });
+        } else {
+            resolve(null);
+        }
+    });
+
+    function createCognitoUser(email) {
+        return new AmazonCognitoIdentity.CognitoUser({
+            Username: email,
+            Pool: userPool
+        });
+    }
+
+
+
+    function signin(email, password, onSuccess, onFailure) {
+        var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails({
+            Username: email,
+            Password: password
+        });
+
+        var cognitoUser = createCognitoUser(email);
+        cognitoUser.authenticateUser(authenticationDetails, {
+            onSuccess: onSuccess,
+            onFailure: onFailure,
+            newPasswordRequired: function (userAttributes) {
+                delete userAttributes.email_verified;
+
+                // Get the new password
+                var newPassword = "";
+
+                do {
+                  newPassword = prompt("Enter your new password with at least 8 characters, 1 uppercase, 1 lowercase, 1 number and 1 special character:");
+                  newPassword = newPassword.trim();
+                } while (newPassword == undefined || newPassword == "");
+
+                cognitoUser.completeNewPasswordChallenge(newPassword, userAttributes, this);
+            }
+        });
+    }
+
+    /*
+     *  Event Handlers
+     */
+
+    $(function onDocReady() {
+        $('#signinForm').submit(handleSignin);
+    });
+
+    function handleSignin(event) {
+        var email = $('#emailInputSignin').val();
+        var password = $('#passwordInputSignin').val();
+        event.preventDefault();
+        signin(email, password,
+            function signinSuccess() {
+                console.log('Successfully Logged In');
+                window.location.href = 'index.html';
+            },
+            function signinError(err) {
+                alert(err);
+            }
+        );
+    }
+}(jQuery));
